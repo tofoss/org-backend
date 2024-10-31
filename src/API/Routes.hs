e51d56799b57a144a2663a6208c671fd557f0e32
Tormod Fossum <tormod.fossum@nav.no>
2024-10-31 14:40:32 +0100

    We have ssl

--- src/API/Routes.hs
+++ src/API/Routes.hs
@@ -7,7 +7,10 @@ import API.Requests.RegisterRequest
 import API.Responses.RegisterResponse
 import Handlers.Users (registerUser)
 import DB.Connection
-import Network.Wai.Handler.Warp (run)
+import Network.Wai.Handler.Warp
+import Network.Wai.Handler.WarpTLS
+import System.Directory (getHomeDirectory)
+import System.FilePath ((</>))
 
 type API = "users" :> "register" :> ReqBody '[JSON] RegisterRequest :> Post '[JSON] RegisterResponse
 
@@ -20,4 +23,9 @@ start = do
     conn <- initDB 
     let port = 8080
     putStrLn $ "Server running on port " ++ show port
-    run port (serve api (registerUser conn))
+    homeDir <- getHomeDirectory
+    let certPath = homeDir </> ".secrets/tls/cert.pem"
+        keyPath  = homeDir </> ".secrets/tls/secret-key.pem"
+    runTLS (tlsSettings certPath keyPath) warpOpts (serve api (registerUser conn))
+    where 
+          warpOpts = setPort 8080 defaultSettings
